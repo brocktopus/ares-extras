872320854186d17cb69ca726cf121df471375e59
Kevin Brock <ikatowi@gmail.com>
2020-02-19 20:49:29 -0500

    adding additional custom classes to db model

--- plugins/traits/plugin/public/traits_char.rb
+++ plugins/traits/plugin/public/traits_char.rb
@@ -1,5 +1,19 @@
 module AresMUSH
-  class Character
-    attribute :traits, :type => DataType::Hash, :default => {}
+  class Character < Ohm::Model
+    collection :traits, "AresMush::SingleTrait"
+
+#    original one-line model info below
+#    attribute :traits, :type => DataType::Hash, :default => {}
   end
+  
+  class SingleTrait < Ohm::Model
+    include ObjectModel
+    
+    attribute :name
+    attribute :category
+    attribute :description
+    reference :character, "AresMUSH::Character"
+    index :name    
+  end
+  
 end
